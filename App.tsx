1a915a3d4f73316b35d4bd626eb8a5f5d371ac8a
chaima <amirou.septiembre@gmail.com>
2025-01-08 19:55:27 +0100

    proyecto inciado

--- App.tsx
+++ App.tsx
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>
+        Inicia sesión para continuar
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
