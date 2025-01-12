7781aec18787738ab3011ba89e5caa02419fdaa6
Chaima <1925046@alu.murciaeduca.es>
2025-01-12 13:31:28 +0100

    cambios de estructura

--- App.tsx
+++ App.tsx
@@ -1,14 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Styles } from './src/theme/Styles';
+import { Redirect } from "expo-router";
 
 export default function App() {
-  return (
-    <View style={Styles.contenedor}>
-      <Text style={Styles.tituloLogin}>Bienvenido a</Text>
-      <Text style={Styles.PetConnect}>PetConnect</Text>
-      
-      <StatusBar style="auto"/>
-    </View>
-  );
+  return <Redirect href="./app"/>;
 }
